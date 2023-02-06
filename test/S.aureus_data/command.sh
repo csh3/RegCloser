75db3fd827a16153e33f38ccf8f899f2dff7e374
Cao Shenghao <cao.shenghao@foxmail.com>
2023-02-06 21:01:11 +0800

    Update command.sh

--- test/S.aureus_data/command.sh
+++ test/S.aureus_data/command.sh
@@ -1,1 +1,5 @@
+# Run RegCloser
 python ~/RegCloser/RunPipeline.py -p prerequisite -g draft_genome.fasta -d output -t 48
+
+# Run Phrap for gap closing
+python ~/RegCloser/RunPipeline_phrap.py -p prerequisite -g draft_genome.fasta -d output_phrap -t 48
